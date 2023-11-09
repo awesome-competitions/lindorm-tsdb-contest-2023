8c116f39c0ed774e92437d9984a654ff0376518d
nico <ainililia@163.com>
2023-11-09 11:07:08 +0800

    feat: 600

--- src/main/java/com/alibaba/lindorm/contest/v2/Const.java
+++ src/main/java/com/alibaba/lindorm/contest/v2/Const.java
@@ -26,7 +26,7 @@ public interface Const {
     int VIN_LENGTH = 17;
 
     // block size
-    int BLOCK_SIZE = 400;
+    int BLOCK_SIZE = 600;
     // the time span of all the data
     int TIME_SPAN = 60 * 60 * 10;
 
